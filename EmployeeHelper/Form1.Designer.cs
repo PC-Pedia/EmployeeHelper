196909c443636fddd4cdfbb7b80c412960525a4d
farzad moomeni <farzad.moomeni@outlook.com>
2019-11-24 09:58:08 +0330

    Add project files.

---
+++ EmployeeHelper/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace EmployeeHelper
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblText = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.tblMain = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tblFooter = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblEach = new System.Windows.Forms.Label();
+            this.txtMinute = new System.Windows.Forms.TextBox();
+            this.btnSetMinute = new System.Windows.Forms.Button();
+            this.lblMinute = new System.Windows.Forms.Label();
+            this.tblMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tblFooter.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblText
+            // 
+            this.lblText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.Location = new System.Drawing.Point(205, 1);
+            this.lblText.Name = "lblText";
+            this.lblText.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblText.Size = new System.Drawing.Size(284, 32);
+            this.lblText.TabIndex = 1;
+            this.lblText.Text = "منتظر باش تا بهت بگم پاشی نرمش کنی";
+            this.lblText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnOK.Location = new System.Drawing.Point(496, 4);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(194, 26);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "باشه متوجه شدم";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Location = new System.Drawing.Point(753, 141);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(34, 122);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(13, 141);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(34, 122);
+            this.btnBack.TabIndex = 4;
+            this.btnBack.Text = "<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // tblMain
+            // 
+            this.tblMain.ColumnCount = 5;
+            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tblMain.Controls.Add(this.btnNext, 3, 2);
+            this.tblMain.Controls.Add(this.btnBack, 1, 2);
+            this.tblMain.Controls.Add(this.pictureBox1, 2, 1);
+            this.tblMain.Controls.Add(this.tblFooter, 2, 4);
+            this.tblMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblMain.Location = new System.Drawing.Point(0, 0);
+            this.tblMain.Name = "tblMain";
+            this.tblMain.RowCount = 6;
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tblMain.Size = new System.Drawing.Size(800, 450);
+            this.tblMain.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::EmployeeHelper.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(53, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.tblMain.SetRowSpan(this.pictureBox1, 3);
+            this.pictureBox1.Size = new System.Drawing.Size(694, 382);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tblFooter
+            // 
+            this.tblFooter.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tblFooter.ColumnCount = 3;
+            this.tblFooter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tblFooter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblFooter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tblFooter.Controls.Add(this.btnOK, 2, 0);
+            this.tblFooter.Controls.Add(this.lblText, 1, 0);
+            this.tblFooter.Controls.Add(this.panel1, 0, 0);
+            this.tblFooter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblFooter.Location = new System.Drawing.Point(53, 401);
+            this.tblFooter.Name = "tblFooter";
+            this.tblFooter.RowCount = 1;
+            this.tblFooter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblFooter.Size = new System.Drawing.Size(694, 34);
+            this.tblFooter.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.lblEach);
+            this.panel1.Controls.Add(this.txtMinute);
+            this.panel1.Controls.Add(this.btnSetMinute);
+            this.panel1.Controls.Add(this.lblMinute);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(194, 26);
+            this.panel1.TabIndex = 6;
+            // 
+            // lblEach
+            // 
+            this.lblEach.AutoSize = true;
+            this.lblEach.Location = new System.Drawing.Point(176, 7);
+            this.lblEach.Name = "lblEach";
+            this.lblEach.Size = new System.Drawing.Size(19, 13);
+            this.lblEach.TabIndex = 3;
+            this.lblEach.Text = "هر";
+            this.lblEach.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtMinute
+            // 
+            this.txtMinute.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMinute.Location = new System.Drawing.Point(110, 0);
+            this.txtMinute.Multiline = true;
+            this.txtMinute.Name = "txtMinute";
+            this.txtMinute.Size = new System.Drawing.Size(60, 26);
+            this.txtMinute.TabIndex = 4;
+            this.txtMinute.Text = "120";
+            // 
+            // btnSetMinute
+            // 
+            this.btnSetMinute.Location = new System.Drawing.Point(3, 0);
+            this.btnSetMinute.Name = "btnSetMinute";
+            this.btnSetMinute.Size = new System.Drawing.Size(60, 26);
+            this.btnSetMinute.TabIndex = 5;
+            this.btnSetMinute.Text = "تنظیم کن";
+            this.btnSetMinute.UseVisualStyleBackColor = true;
+            this.btnSetMinute.Click += new System.EventHandler(this.btnSetMinute_Click);
+            // 
+            // lblMinute
+            // 
+            this.lblMinute.AutoSize = true;
+            this.lblMinute.Location = new System.Drawing.Point(69, 7);
+            this.lblMinute.Name = "lblMinute";
+            this.lblMinute.Size = new System.Drawing.Size(35, 13);
+            this.lblMinute.TabIndex = 3;
+            this.lblMinute.Text = " دقیقه";
+            this.lblMinute.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tblMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "برنامه جلوگیری از سنگ شدن کارمندان";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.tblMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tblFooter.ResumeLayout(false);
+            this.tblFooter.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.TableLayoutPanel tblMain;
+        private System.Windows.Forms.TableLayoutPanel tblFooter;
+        private System.Windows.Forms.Label lblMinute;
+        private System.Windows.Forms.TextBox txtMinute;
+        private System.Windows.Forms.Button btnSetMinute;
+        private System.Windows.Forms.Label lblEach;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ EmployeeHelper/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EmployeeHelper
+{
+    public partial class frmMain : Form
+    {
+        private DateTime _Dt;
+        private bool _OkFlag = false;
+        private int _PicNumber = 1;
+        private TimeSpan _Ts;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            timer1.Start();
+            _Dt = DateTime.Now;
+            txtMinute.Text = Properties.Settings.Default.minute;
+            _Ts = new TimeSpan(0, int.Parse(Properties.Settings.Default.minute), 0);
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (_PicNumber > 0)
+            {
+                _PicNumber -= 1;
+            }
+
+            if (_PicNumber == 0)
+            {
+                _PicNumber = 6;
+            }
+
+            ChangePicture(_PicNumber);
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (_PicNumber < 7)
+            {
+                _PicNumber += 1;
+            }
+
+            if (_PicNumber == 7)
+            {
+                _PicNumber = 1;
+            }
+
+            ChangePicture(_PicNumber);
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            _OkFlag = true;
+
+            _Ts = new TimeSpan(0, int.Parse(Properties.Settings.Default.minute), 0);
+            _Dt = DateTime.Now;
+
+            lblText.Text = "منتظر باش تا بهت بگم پاشی نرمش کنی";
+            btnOK.Enabled = false;
+            this.TopMost = false;
+        }
+
+        private void btnSetMinute_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtMinute.Text, out int a))
+            {
+                Properties.Settings.Default.minute = txtMinute.Text;
+                Properties.Settings.Default.Save();
+
+                _Ts = new TimeSpan(0, int.Parse(Properties.Settings.Default.minute), 0);
+                _Dt = DateTime.Now;
+
+                MessageBox.Show(string.Format("تنظیم شد و از الان هر {0} دقیقه یکبار برنامه باخبرت میکنه که نرمش کنی", txtMinute.Text));
+            }
+            else
+            {
+                MessageBox.Show("فقط عدد وارد کنید");
+            }
+        }
+
+        private void ChangePicture(int picNumber)
+        {
+            switch (picNumber)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources._1;
+                    break;
+
+                case 2:
+                    pictureBox1.Image = Properties.Resources._2;
+                    break;
+
+                case 3:
+                    pictureBox1.Image = Properties.Resources._3;
+                    break;
+
+                case 4:
+                    pictureBox1.Image = Properties.Resources._4;
+                    break;
+
+                case 5:
+                    pictureBox1.Image = Properties.Resources._5;
+                    break;
+
+                case 6:
+                    pictureBox1.Image = Properties.Resources._6;
+                    break;
+            }
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("سنگ میشی اگه برنامه رو ببندی. میخوای برنامه رو ببندی؟", "توجه", MessageBoxButtons.OKCancel);
+            if (dr == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void ShakeIt()
+        {
+            this.Top = this.Top + 10;
+            System.Threading.Thread.Sleep(50);
+            this.Top = this.Top - 10;
+            System.Threading.Thread.Sleep(50);
+            this.Top = this.Top + 10;
+            System.Threading.Thread.Sleep(50);
+            this.Top = this.Top - 10;
+            System.Threading.Thread.Sleep(50);
+            this.Top = this.Top + 10;
+            System.Threading.Thread.Sleep(50);
+            this.Top = this.Top - 10;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int minute = DateTime.Now.Subtract(_Dt).Minutes;
+
+            int savedTime = int.Parse(Properties.Settings.Default.minute);
+
+            if (minute >= savedTime)
+            {
+                btnOK.Text = "باشه متوجه شدم";
+
+                if (_OkFlag == false)
+                {
+                    lblText.Text = "پاشو پاشو پاشو پاشو پاشو پاشو";
+                    this.TopMost = true;
+
+                    if (this.WindowState == FormWindowState.Minimized)
+                    {
+                        this.WindowState = FormWindowState.Normal;
+                    }
+
+                    btnOK.Enabled = true;
+                    Console.Beep();
+                    ShakeIt();
+                    this.Update();
+                }
+            }
+            else
+            {
+                TimeSpan ts = new TimeSpan(0, 0, 1);
+                _Ts = _Ts.Subtract(ts);
+                btnOK.Text = string.Format("{0}:{1}:{2}",_Ts.Hours.ToString(), _Ts.Minutes.ToString(), _Ts.Seconds.ToString());
+
+                _OkFlag = false;
+                lblText.Text = "منتظر باش تا بهت بگم پاشی نرمش کنی";
+                this.TopMost = false;
+                btnOK.Enabled = false;
+                this.Update();
+            }
+        }
+    }
+}
